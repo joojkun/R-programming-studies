d5b1a0fd877e1e796af2e49485e3db3862a42bcc
joojkun <johnn_43@outlook.com>
2023-09-20 19:33:29 -0300

    Valores booleanos e Laços de Repetição

---
+++ 4 - Booleans/booleans.r
@@ -0,0 +1,22 @@
+# Comparação de valores
+10 > 9
+10 == 9
+10 < 9
+
+# Comparação de variáveis
+var1 <- 9
+var2 <- 23
+
+var1 > var2
+var1 == var2
+var1 < var2
+
+# Comparação entre tipos de variáveis
+class(var1) == class(var2)
+
+# Estruturas condicionais
+if (var1 > var2) {
+  paste(var1, " é maior do que ", var2)
+} else {
+  paste(var2, " é maior do que ", var1)
+}
---
+++ 5 - Loops/loops.r
@@ -0,0 +1,12 @@
+# Estruturas de repetição
+var1 <- list(3, 6, 2, 6) # Lista de valores
+
+for (item in var1) {
+  print(item)
+}
+
+var2  <- 0
+while (var2 < 10) {
+  print(var2)
+  var2 <- var2 + 1
+}
